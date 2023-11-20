405976a3f471bf1018030793658149580d375ab7
CaiozinAlves <129068701+CaiozinAlves@users.noreply.github.com>
2023-11-20 10:35:44 -0300

    Add files via upload

---
+++ exercicio14/exercicio08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ main() {
+    float array[10]; 
+
+    
+    printf("Endereços de cada posição do array:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Posição %d: %p\n", i, (void*)&array[i]);
+    }
+
+    
+}
